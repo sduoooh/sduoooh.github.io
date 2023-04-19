9609d388fd70c09f4443823f76acc8cccb2a9ae1
sduoooh <96587057+sduoooh@users.noreply.github.com>
2023-04-19 23:19:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 import { ref } from "/littleVue.js"
    
+let a = document.getElementById('app')
+const clickEvent = () => {
+  if (a) {
+    a.innerHTML = parseInt(a.innerHTML) + 1
+  }
+}
+
+
+let input1 = 0
+let input = document.getElementById('input')
+input.addEventListener('input', (e) => {
+  if ((/^[0-9]+$/).test(e.target.value)) {
+    document.getElementById('app').innerHTML = parseInt(e.target.value)
+  }
+  else {
+    document.getElementById('app').innerHTML = 0
+  }
+})
+
 const ok = ref(0, {
       address: 'app1',
       function: (value) => {
